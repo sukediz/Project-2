5be95fc0023b395423bb3e414ef76c0090be4253
Sukey Ong <63219425+sukediz@users.noreply.github.com>
2020-08-17 21:01:01 +0800

    Revert "Feature/um.10.10 create user registration"

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const nameInput = $("input#name-input");
   const emailInput = $("input#email-input");
+  const phoneInput = $("input#phone-input");
   const cityInput = $("input#city-input");
   const passwordInput = $("input#password-input");
 
@@ -12,6 +13,7 @@ $(document).ready(() => {
     const userData = {
       name: nameInput.val().trim(),
       email: emailInput.val().trim(),
+      phone: phoneInput.val().trim(),
       city: cityInput.val().trim(),
       password: passwordInput.val().trim()
     };
@@ -31,6 +33,7 @@ $(document).ready(() => {
     $.post("/api/signup", {
       name: name,
       email: email,
+      phone: phone,
       city: city,
       password: password
     })
